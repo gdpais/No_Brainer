08051e5750e21f2b44471fef795bccc556245845
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-06-09 21:54:16 +0100

    Telekinesis update (Range)

--- No_Brainer/Assets/Scripts/FollowMouse.cs
+++ No_Brainer/Assets/Scripts/FollowMouse.cs
@@ -10,6 +10,8 @@ public class FollowMouse : MonoBehaviour, IPointerDownHandler
     private Rigidbody2D rb;
     private Vector2 position = new Vector2(-0.98f, 8.3f);
     private bool canMove;
+
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,17 +20,24 @@ public class FollowMouse : MonoBehaviour, IPointerDownHandler
 
     private void Update()
     {
+
+        Vector2 pointOnScreen = Camera.main.WorldToScreenPoint(GetComponentInChildren<Renderer>().bounds.center);
         // GameObject.FindGameObjectWithTag("Your_Tag_Here").transform.position;
-        if (Input.GetMouseButton(0) && canMove)
+        if ((pointOnScreen.x > 0) && (pointOnScreen.x < Screen.width) &&
+                        (pointOnScreen.y > 0) && (pointOnScreen.y < Screen.height))
         {
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-            rb.MovePosition(position);
+            if (Input.GetMouseButton(0) && canMove)
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+                rb.MovePosition(position);
+            }
         }
+
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
+        //Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
         canMove = !canMove;
     }
 
