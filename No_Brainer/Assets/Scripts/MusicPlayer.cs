4175cd829dadb49c41ac63dea473f4f8376595a6
André Serrado <58612578+AndreSerrado@users.noreply.github.com>
2021-06-12 23:39:01 +0100

    Musica e volume

--- No_Brainer/Assets/Scripts/MusicPlayer.cs
+++ No_Brainer/Assets/Scripts/MusicPlayer.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayer : MonoBehaviour
 {
-    private AudioSource  audioSource;
-    private float musicVolume  = 1f;
+    public Slider volumeSlider;
+    public AudioSource audioSource;
+    private float musicVolume = 1f;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        musicVolume = PlayerPrefs.GetFloat("volume");
+        audioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
     }
 
     void Update()
     {
         audioSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
     }
 
     public void updateVolume(float volume)
