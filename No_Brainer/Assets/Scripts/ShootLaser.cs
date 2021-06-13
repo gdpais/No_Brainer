39486fd3e0e9bb0f12402e20b63a48afc973ae2f
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-06-13 02:14:23 +0100

    Doors colliders update

--- No_Brainer/Assets/Scripts/ShootLaser.cs
+++ No_Brainer/Assets/Scripts/ShootLaser.cs
@@ -16,7 +16,7 @@ public class ShootLaser : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         beam.DestroyLaser();
         beam.SetProperties(gameObject.transform.position, (toLeft * gameObject.transform.right));
