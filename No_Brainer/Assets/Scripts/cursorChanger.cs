226c271c8adadf7bae72a356b77ed724caeb82b4
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-06-13 13:32:14 +0100

    Cursor fix

--- No_Brainer/Assets/Scripts/cursorChanger.cs
+++ No_Brainer/Assets/Scripts/cursorChanger.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class cursorChanger : MonoBehaviour
 {
+    private Vector2 hotSpot = Vector2.zero;
+    public Texture2D cursorTexture;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.ForceSoftware);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = cursorPos;
-
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = cursorPos;
     }
 }
