ca57a966a66d67be4ffcc5f69056f3b171272830
André Serrado <58612578+AndreSerrado@users.noreply.github.com>
2021-06-13 04:26:53 +0100

    Menu Pausa do jogo

---
+++ No_Brainer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        AudioListener.pause = false;
+    }
+
+    public void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        AudioListener.pause = true;
+    }
+
+    public void restart()
+    {
+        resume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void exitGame()
+    {
+        resume();
+        SceneManager.LoadScene("menu");
+        //AudioSource audio = GetComponent<AudioSource>();
+        //Destroy(audio);
+    }
+}
