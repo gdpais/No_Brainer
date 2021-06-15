ab57934070076224444826d2abd75dfe9e9dd15b
Gabriel-AlvesP <190221057@estudantes.ips.pt>
2021-06-15 19:22:11 +0100

    last door script

---
+++ No_Brainer/Assets/Scripts/ReceptorsHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ReceptorsHandler : MonoBehaviour
+{
+    private static int NUMBER_OF_RECEPTORS = 2;
+    public GameObject[] receptorsList = new GameObject[NUMBER_OF_RECEPTORS];
+    public GameObject door;
+    private bool canOpen;
+    void Start()
+    {
+        canOpen = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CanOpen();
+        if (canOpen)
+        {
+            GetComponent<Animator>().SetBool("redToGreen", true);
+            door.GetComponent<Animator>().SetBool("Close", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("redToGreen", false);
+            door.GetComponent<Animator>().SetBool("Close", false);
+        }
+    }
+
+    private void CanOpen()
+    {
+        canOpen = true;
+        foreach (GameObject receptor in receptorsList)
+        {
+            if (!receptor.GetComponent<Animator>().GetBool("isOn"))
+                canOpen = false;
+        }
+
+    }
+}
