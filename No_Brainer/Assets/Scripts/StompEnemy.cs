358ec58c087630fde5adcdc894c9e285eb4f099d
André Serrado <58612578+AndreSerrado@users.noreply.github.com>
2021-07-10 18:56:00 +0100

    Sons

--- No_Brainer/Assets/Scripts/StompEnemy.cs
+++ No_Brainer/Assets/Scripts/StompEnemy.cs
@@ -4,11 +4,13 @@ public class StompEnemy : MonoBehaviour
 {
     public float force = 10.0f;
     bool stomped = false;
+    public AudioClip deathSound;
     // Start is called before the first frame updates
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(deathSound, transform.position);
             Rigidbody2D palyerRb = other.GetComponent<Rigidbody2D>();
             palyerRb.AddForce(transform.up * force, ForceMode2D.Impulse);
             stomped = true;
