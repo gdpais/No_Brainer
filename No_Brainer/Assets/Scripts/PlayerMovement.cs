211586148f22ee6863b029670cea601d263499a7
Gabriel-AlvesP <72794818+Gabriel-AlvesP@users.noreply.github.com>
2021-07-13 15:23:05 +0100

    Virus bug fix + restart after dying fix
    virus now returns when it leaves his zone
    Restart menu now appears (again)

--- No_Brainer/Assets/Scripts/PlayerMovement.cs
+++ No_Brainer/Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     public AudioClip deathSound;
 
 
+
     private void Start()
     {
         score = 0;
--- No_Brainer/Assets/Scripts/VirusController.cs
+++ No_Brainer/Assets/Scripts/VirusController.cs
@@ -7,12 +7,40 @@ public class VirusController : MonoBehaviour
     public float minLimit = 21.0f;
     public float maxLimit = 31.0f;
     private int direction = 1;
+    private bool isReturning;
 
+    private void Start()
+    {
+        isReturning = false;
+    }
     // Update is called once per frame
     void Update()
     {
+
+
+
+    }
+    private void FixedUpdate()
+    {
         transform.Translate(direction * Vector2.right * Time.deltaTime);
-        if (transform.position.x <= minLimit || transform.position.x >= maxLimit)
-            direction *= -1;
+        if (transform.position.x < minLimit || transform.position.x > maxLimit)
+        {
+            if (!isReturning)
+            {
+                direction *= -1;
+                isReturning = true;
+            }
+        }
+
+        if (transform.position.x >= minLimit && transform.position.x <= maxLimit)
+        {
+            isReturning = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        direction *= -1;
     }
 }
